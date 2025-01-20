ed6c8ce0fd162f739df8e73c87277b7172269fda
dough10 <doughten.jimi@gmail.com>
2025-01-20 16:53:17 -0600

    settings

---
+++ config/settings.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  "app" => [
+    "encoding" => "utf-8",
+    "language" => "en-US",
+    "file-path" => "/downloads"
+  ],
+  "database" => [ 
+    "dsn" => "sqlite: /downloads/dl-db.db"
+  ]
+];
