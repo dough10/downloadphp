8079c550eece472d2fe7aebcb63f5e330260075c
dough10 <doughten.jimi@gmail.com>
2025-01-19 19:00:02 -0600

    idk

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $username = 'dough10';
 
-$dbFile = '../' . $username . '_downloads.db';
+$dbFile = $username . '_downloads.db';
 
 $database = new Db($dbFile);
 
