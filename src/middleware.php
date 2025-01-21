d3750d3b50739c6aa1cc3c198f854fd11714c410
dough10 <doughten.jimi@gmail.com>
2025-01-21 17:11:08 -0600

    limit settings from config

--- src/middleware.php
+++ src/middleware.php
@@ -5,18 +5,21 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use App\Helpers;
 
 return function (App $app) {
-  $app->add(function (Request $request, RequestHandler $next) {
-    $rateLimitWindow = 60;
-    $maxRequests = 20;
+  $container = $app->getContainer();
+  $logger = $container->get('logger');
+  
+  $app->add(function (Request $request, RequestHandler $next) use ($logger) {
+    $settings = require __DIR__ . '/../config/settings.php';
     if (!isset($_SESSION['request_count'])) {
       $_SESSION['request_count'] = 0;
       $_SESSION['first_request_time'] = time();
     }
-    if (time() - $_SESSION['first_request_time'] > $rateLimitWindow) {
+    if (time() - $_SESSION['first_request_time'] > $settings['limit']['limit-window']) {
       $_SESSION['request_count'] = 0;
       $_SESSION['first_request_time'] = time();
     }
-    if ($_SESSION['request_count'] >= $maxRequests) {
+    if ($_SESSION['request_count'] >= $settings['limit']['max-requests']) {
+      $logger->notice(Helpers\getUserIP() . ' ' . $_SESSION['username'] . ' has hit the rate limit');
       $response = new \Slim\Psr7\Response();
       $response->getBody()->write(json_encode(['error' => 'Rate limit exceeded. Please try again later.']));
       return $response->withStatus(429)->withHeader('Content-Type', 'application/json');
