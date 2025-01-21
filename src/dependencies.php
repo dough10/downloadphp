a4c0cd0a32f1934da4fedd68c4aa47bfa0d6305f
dough10 <doughten.jimi@gmail.com>
2025-01-21 16:48:48 -0600

    nore to the point

--- src/dependencies.php
+++ src/dependencies.php
@@ -8,9 +8,9 @@ use Monolog\Formatter\LineFormatter;
 return function (App $app) {
   $settings = require __DIR__ . '/../config/settings.php';
   $container = $app->getContainer();
-  $container->set('database', function (): Db {
-    return new Db();
-  });
+
+  $container->set('database', new Db());
+
   $container->set('logger', function () use ($settings): Logger {
     $logger = new Logger('downloadphp');
     $stream = new StreamHandler(__DIR__ . "/../logs/downloadphp.log", $settings['app']['log-level']);
