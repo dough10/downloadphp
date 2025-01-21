71ab4d2c2966af1e1e6bb0b8b44b5dd3ab25c8be
dough10 <doughten.jimi@gmail.com>
2025-01-21 17:10:49 -0600

    moved

--- src/dependencies.php
+++ src/dependencies.php
@@ -13,7 +13,7 @@ return function (App $app) {
 
   $container->set('logger', function () use ($settings): Logger {
     $logger = new Logger('downloadphp');
-    $stream = new StreamHandler($settings['app']['log-location'], $settings['app']['log-level']);
+    $stream = new StreamHandler($settings['log']['log-location'], $settings['log']['log-level']);
     $logger->pushHandler($stream);
     $logger->getHandlers()[0]->setFormatter(new LineFormatter(
       "%datetime% %level_name%: %message% %context% %extra%\n",
