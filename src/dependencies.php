d6554cc5f4702fa5fa089b02021bcbea1bfe79fd
dough10 <doughten.jimi@gmail.com>
2025-01-21 08:41:33 -0600

    add a logger to the container

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,10 +1,19 @@
 <?php
 use Slim\App;
 use App\Models\Db;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 return function (App $app) {
+  $settings = require __DIR__ . '/../config/settings.php';
   $container = $app->getContainer();
   $container->set('database', function (): Db {
     return new Db();
+  });
+  $container->set('logger', function () use ($settings): Logger {
+    $logger = new Logger('downloadphp');
+    $stream = new StreamHandler(__DIR__ . "/../logs/downloadphp.log", $settings['app']['log-level']);
+    $logger->pushHandler($stream);
+    return $logger;
   });
 };
