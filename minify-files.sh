420c51b4aa29ae0f6074927c9ca5edfb9d1d1dd6
dough10 <doughten.jimi@gmail.com>
2025-01-21 18:55:14 -0600

    more minifying of things

--- minify-files.sh
+++ minify-files.sh
@@ -1,4 +1,33 @@
 #!/usr/bin/bash
+
+generate_random_string() {
+  length=$1
+  openssl rand -base64 "$length" | tr -d '/+' | cut -c1-"$length"
+}
+
+replace_path() {
+  search="$1"
+  replace="$2"
+  file="$3"
+
+  search_escaped=$(printf '%s' "$search" | sed 's/[&/\]/\\&/g')
+  replace_escaped=$(printf '%s' "$replace" | sed 's/[&/\]/\\&/g')
+
+  sed -i "" "s/$search_escaped/$replace_escaped/g" "$file"
+}
+
+version=$(generate_random_string 8)
+
+css_path=css/base.min.$version.css
+js_path=js/app.min.$version.js
+
+rm public/css/*.css
+rm public/js/*.js
+
 html-minifier html-src/downloads.phtml --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype  --output public/downloads.phtml
-uglifycss html-src/css/base.css --output public/css/base.min.css
-uglifyjs html-src/js/app.js --output public/js/app.min.js
+uglifycss html-src/css/base.css --output "public/$css_path"
+uglifyjs html-src/js/app.js --output "public/$js_path"
+
+replace_path ./css/base.css "./$css_path" public/downloads.phtml
+replace_path ./js/app.js "./$js_path" public/downloads.phtml
+
