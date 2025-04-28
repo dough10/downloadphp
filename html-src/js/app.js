5c085b6105f7bfa2dca06b3bd4bdd079ca96de4a
dough10 <doughten.jimi@gmail.com>
2025-04-28 04:51:44 -0500

    refactor: reorganize code structure and improve download handling logic

--- html-src/js/app.js
+++ html-src/js/app.js
@@ -2,25 +2,17 @@ import Toast from "./Toast/Toast.js";
 import UIManager from "./UIManager/UIManager.js";
 import DownloadManager from "./DownloadManager/DownloadManager.js";
 import EventManager from "./utils/EventManager/EventManager.js";
-
-import sleep from "./utils/sleep.js";
 import selectors from "./utils/selectors.js";
+import init from './dialog/dialog.js';
 
-import init from "./dialog/dialog.js";
-
-const uiManager = new UIManager();
-const downloadManager = new DownloadManager();
 const em = new EventManager();
+const downloadManager = new DownloadManager();
+const uiManager = new UIManager();
 
 
 const eventNamespaces = {
   DOWNLOAD: 'download'
-}
-
-/**
- * List of files actively being downloaded
- */
-let activedownloads = []; 
+};
 
 /**
  * makes a pending file as complete in download logs
@@ -33,7 +25,7 @@ async function logCompleted(name, ndx, status) {
   try {
     const updates = await downloadManager.markCompleted(name, ndx, status);
     const htmlElements = updates.map(uiManager.createLogEntry);
-    document.querySelector('#history>ul').replaceChildren(...htmlElements);      
+    document.querySelector(selectors.historyList).replaceChildren(...htmlElements);      
   } catch(error) {
     console.error(error)
   }
@@ -52,19 +44,35 @@ async function saveFile(chunks, name, ndx) {
   link.download = name;
   link.click();
   URL.revokeObjectURL(link.href);
-  logCompleted(name, ndx, true);
 }
 
-function downloadFinished(update, name, ndx, row) {
+/**
+ * a download finished (failed, canceled, complete)
+ * 
+ * @param {Object} update
+ * @param {Object} update.detail
+ * @param {Array} update.detail.chunks 
+ * @param {String} name 
+ * @param {Number} ndx 
+ */
+function downloadFinished(update, name, ndx) {
   const {chunks} = update.detail;
   saveFile(chunks, name, ndx);
   em.removeByNamespace(eventNamespaces.DOWNLOAD);
-  uiManager.downloadEnded(row, 'Download complete.');
+  uiManager.downloadEnded(ndx, 'Download complete.');
+  logCompleted(name, ndx, true);
 }
 
-function userStopped(row) {
+/**
+ * user stopped the download
+ * 
+ * @param {String} name 
+ * @param {Number} ndx 
+ */
+function userStopped(name, ndx) {
   em.removeByNamespace(eventNamespaces.DOWNLOAD);
-  uiManager.downloadEnded(row, 'Download stopped by user');
+  uiManager.downloadEnded(ndx, 'Download stopped by user');
+  logCompleted(name, ndx, 'canceled');
 }
 
 /**
@@ -87,25 +95,21 @@ async function download({path, name, ndx}) {
   new Toast(`Downloading: ${name}`, 1);
 
   try {
-    const fileDownload = await downloadManager.getFile(path);
+    const fileDownload = await downloadManager.getFile(path, ndx);
 
-    const {row, dlSpeed, bar} = uiManager.createDownloadUI(name, _ => {
-      em.removeByNamespace(ns);
+    const {dlSpeed, bar} = uiManager.createDownloadUI(name, _ => {
+      if (!fileDownload.downloading) return;
       fileDownload.stop();
-      uiManager.setButtonDisabledState(selectors.clearHistoryButton, false);
-      uiManager.cleanupDownload(row);
     }, ndx);
     
     em.add(fileDownload, 'update', update => uiManager.progressUpdated(update, bar, dlSpeed), ns);
-    em.add(fileDownload, 'finished', update => downloadFinished(update, name, ndx, row), ns);
-    em.add(fileDownload, 'stopped', _ => userStopped(row), ns);
+    em.add(fileDownload, 'finished', update => downloadFinished(update, name, ndx), ns);
+    em.add(fileDownload, 'stopped', _ => userStopped(name, ndx), ns);
     
     fileDownload.start();
   } catch(error) {
-    const errorMessage = (error.name === 'AbortError') ? 'Download canceled.' : `Failed to fetch ${path}`;
-    new Toast(errorMessage);
-    // console.error(error);
-    await sleep(2000);
+    const errMessage = (error.name === 'AbortError') ? 'Download canceled.' : `Failed to fetch ${path}`;
+    new Toast(errMessage);
   }
 }
 
@@ -117,10 +121,11 @@ async function download({path, name, ndx}) {
  * @returns {Boolean}
  */
 async function recordDownload(file) {
+  document.querySelector(selectors.historySVG).classList.add('spin');
   const {downloads} = await downloadManager.recordDownload(file);
   const elementList = downloads.map(uiManager.createLogEntry);
   elementList.reverse();
-  const historyList = document.querySelector('#history>ul');
+  const historyList = document.querySelector(selectors.historyList);
   historyList.replaceChildren(...elementList);
   console.log(`${downloads.length} download(s) logged`);
   return elementList[0].dataset.ndx;
@@ -134,71 +139,22 @@ async function recordDownload(file) {
  * @returns {void}
  */
 async function fileClicked(file) {
-  document.querySelector('#hist_but>svg').classList.add('spin');
   const exists = await recordDownload(file.dataset.name);
   if (!exists) {
+    document.querySelector(selectors.historySVG).classList.remove('spin');
     return;
   }
   file.dataset.ndx = exists;
   await download({...file.dataset});
-  document.querySelector('#hist_but>svg').classList.remove('spin');
-}
-
-
-
-
-
-/**
- * checks for avtive downloads before navigating away from page
- * 
- * @param {Event} event 
- * 
- * @returns {String}
- */
-function checkActiveDownloads(event) {
-  event.preventDefault();
-  event.returnValue = '';
-
-  if (!activedownloads.length) {
-    em.removeAll();
-    return;
-  }
-
-  const confirmed = confirm('Download(s) active. Are you sure you want to leave?');
-  console.log(confirmed);
-  if (confirmed) {
-    activedownloads.forEach(dl => logCompleted(dl.name, dl.ndx, 'canceled'));
-    activedownloads = [];
-    window.location.href = event.target.href || window.location.href;
-    em.removeAll();
-  }
 }
 
 /**
- * app loaded callback
+ * load application
  */
-function appLoaded() {
+function loaded() {
   init();
-
-  const files = document.querySelectorAll(selectors.file);
-  files.forEach(file => {
-    em.add(file, 'click', _ => fileClicked(file))
-    em.add(file, 'keydown', (event) => {
-      if (event.key === 'Enter' || event.key === 'Space') {
-        event.preventDefault();
-        file.click();
-      }
-    });
-  });
-
-  const toTop = document.querySelector(selectors.toTop);
-  em.add(toTop, 'click', _ => document.documentElement.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  }));
-  
-  em.add(document, 'scroll', uiManager.documentScrolled);
+  uiManager.init(fileClicked);
 }
 
-em.add(window, 'load', appLoaded);
-// em.add(window, 'beforeunload', checkActiveDownloads); 
+em.add(window, 'load', loaded);
+em.add(window, 'beforeunload', em.removeAll); 
