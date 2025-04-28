aca289a0d66616c36c34e676c99cb3a49ab0ae27
dough10 <doughten.jimi@gmail.com>
2025-04-28 13:45:36 -0500

    refactor: streamline user path handling in route definitions

--- src/routes.php
+++ src/routes.php
@@ -12,9 +12,9 @@ return function (App $app) {
   $container = $app->getContainer();
   $database = $container->get('database');
   $logger = $container->get('logger');
+  $userPath = $settings['app']['file-path'] . '/' . $_SESSION['username'];
 
-  $app->get('/files/{file}', function (Request $request, Response $response, $args) use ($settings, $logger) {
-    $userPath = $settings['app']['file-path'] . '/' . $_SESSION['username'];
+  $app->get('/files/{file}', function (Request $request, Response $response, $args) use ($userPath, $logger) {
     $file = $userPath . '/' . basename($args['file']);
 
     if (realpath($file) === false || strpos(realpath($file), realpath($userPath)) !== 0) {
@@ -42,8 +42,7 @@ return function (App $app) {
     return $response->withStatus(200);
   });
 
-  $app->post('/request-file/{file}', function (Request $request, Response $response, $args) use ($settings, $database, $logger) {
-    $userPath = $settings['app']['file-path'] . '/' . $_SESSION['username'];
+  $app->post('/request-file/{file}', function (Request $request, Response $response, $args) use ($userPath, $database, $logger) {
     $file = $userPath . '/' . basename($args['file']);
     if (realpath($file) === false || strpos(realpath($file), realpath($userPath)) !== 0) {
       $logger->warning('Forbidden access: ' . $file);
@@ -84,8 +83,7 @@ return function (App $app) {
     }
   });
 
-  $app->get('/', function (Request $request, Response $response, $args) use ($settings, $database, $logger) {
-    $userPath = $settings['app']['file-path'] . '/' . $_SESSION['username'];
+  $app->get('/', function (Request $request, Response $response, $args) use ($settings, $database, $logger, $userPath) {
     try {
       $renderer = new PhpRenderer(__DIR__ . '/../templates');
       $viewData = [
